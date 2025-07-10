10372655a0528a413232e19024d99fc7e07bd191
vaishakhpm <pmvaishakh3@gmail.com>
2025-07-10 21:39:28 +0530

    JsonPath

---
+++ src/test/java/JsonPathEgs/JsonPathForSimpleJsonObject.java
@@ -0,0 +1,13 @@
+package JsonPathEgs;
+
+public class JsonPathForSimpleJsonObject {
+
+	String jsonObject = "{\\r\\n\" +\n"
+			+ "		        \" \\\"firstname\\\": \\\"Amod\\\",\\r\\n \" +\n"
+			+ "		        \" \\\"lastname\\\": \\\"Brown\\\",\\r\\n\" +\n"
+			+ "		        \"}\";\n"
+			+ "		"
+
+	}
+
+}
