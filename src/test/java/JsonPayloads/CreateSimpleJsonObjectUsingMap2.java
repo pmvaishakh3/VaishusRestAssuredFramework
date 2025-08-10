193ab189eb352027fa35ab535f71c0e9e8e45f34
vaishakhpm <pmvaishakh3@gmail.com>
2025-08-10 20:31:18 +0530

    nested Json

---
+++ src/test/java/JsonPayloads/CreateSimpleJsonObjectUsingMap2.java
@@ -0,0 +1,36 @@
+package JsonPayloads;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import io.restassured.RestAssured;
+
+public class CreateSimpleJsonObjectUsingMap2 {
+	
+	private static void main(String[] args) {
+		Map<Object,Object> jsonObjectPayload = new LinkedHashMap< >();
+		jsonObjectPayload.put("id",100);
+		jsonObjectPayload.put("firstname","Sally");
+		jsonObjectPayload.put("lastname","Brown");
+		jsonObjectPayload.put("married",false);
+		jsonObjectPayload.put("salary",100.899);
+		
+		Map<String,Object> addressMap = new LinkedHashMap<String, Object>();
+		addressMap.put("no","31#");
+		addressMap.put("area","kannur");
+		addressMap.put("district","payanyr");
+		addressMap.put("phone","8979999");
+		addressMap.put("state","KA");
+		
+		jsonObjectPayload.put("address","addressMap");
+		
+		RestAssured 
+		.given()
+		.log()
+		.all()
+		.body(jsonObjectPayload)
+		.get();
+
+
+}
+}
